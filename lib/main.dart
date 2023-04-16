22bdb97430c859c5edc1d268cc1cba67099896b8
Rahat <rahat.munta@gmail.com>
2023-04-16 18:10:39 +0600

    change logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:signature/signature.dart';
 
 void main() {
@@ -28,74 +29,124 @@ class MyHomePage extends StatelessWidget {
     var pointList_1 = [];
     var pointList_2 = [];
     var signature;
-    //SignatureController signatureController = SignatureController();
-    //signature =  signatureController.;
-    SignatureController signatureController = SignatureController();
+    //SignatureController signatureController_1 = SignatureController();
+    //signature =  signatureController_1.;
+    SignatureController signatureController_1 = SignatureController();
+    SignatureController signatureController_2 = SignatureController();
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
           title: const Text("Signature Test"),
         ),
-        body: Column(
-          children: [
-            Material(
-              shape: const RoundedRectangleBorder(side: BorderSide(color: Colors.blue, width: 2)),
-              child: Signature(
-                controller: signatureController = SignatureController(onDrawEnd: () {
-                  signature = signatureController.points;
-                  for(var elements in signature){
-                    pointList_1.add([elements.offset.dx.toStringAsFixed(2),elements.offset.dy.toStringAsFixed(2)]);
-                  }
-                  pointList_2.add(pointList_1);
-                }),
-                backgroundColor: Colors.white,
-                height: 500,
-                width: double.infinity,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Material(
+                shape: const RoundedRectangleBorder(side: BorderSide(color: Colors.blue, width: 2)),
+                child: Signature(
+                  controller: signatureController_1 = SignatureController(onDrawEnd: () {
+                    signature = signatureController_1.points;
+                    for (var elements in signature) {
+                      pointList_1.add([elements.offset.dx.toStringAsFixed(2), elements.offset.dy.toStringAsFixed(2)]);
+                    }
+                    pointList_2.add(pointList_1);
+                  }),
+                  backgroundColor: Colors.white,
+                  height: 300,
+                  width: double.infinity,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Material(
-                    color: Colors.green,
-                    child: SizedBox(
-                      height: 40,
-                      child: InkWell(
-                          onTap: () {
-                            signature.clear();
-                            pointList_1.clear();
-                            pointList_2.clear();
-                            signatureController.clear();
-                          },
-                          child: const Text("Tap To clear Screen")),
+              const SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Material(
+                      color: Colors.green,
+                      child: SizedBox(
+                        height: 40,
+                        child: InkWell(
+                            onTap: () {
+                              signature.clear();
+                              pointList_1.clear();
+                              pointList_2.clear();
+                              signatureController_1.clear();
+                            },
+                            child: const Text("Tap To clear Screen")),
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Expanded(
-                  child: Material(
-                    color: Colors.green,
-                    child: SizedBox(
-                      height: 40,
-                      child: InkWell(
-                          onTap: () {
-                            print("****************************************DATA#########################");
-                            print(pointList_2.length);
-                          },
-                          child: const Text("Tap To Show Data")),
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Material(
+                      color: Colors.green,
+                      child: SizedBox(
+                        height: 40,
+                        child: InkWell(
+                            onTap: () {
+                              print("****************************************DATA#########################");
+                              print(pointList_2);
+                            },
+                            child: const Text("Tap To Show Data")),
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Expanded(
+                    child: Material(
+                      color: Colors.green,
+                      child: SizedBox(
+                        height: 40,
+                        child: InkWell(
+                            onTap: () {
+                              signatureController_2 = SignatureController(
+                                points: collectedPoint(listPoint: pointList_2),
+                                penStrokeWidth: 2,
+                                penColor: Colors.red,
+                                exportBackgroundColor: Colors.blue,
+                              );
+                            },
+                            child: const Text("Tap To Copy Signature")),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              Material(
+                  shape: const RoundedRectangleBorder(side: BorderSide(color: Colors.blue, width: 2)),
+                  child: Signature(
+                    controller: signatureController_2,
+                    backgroundColor: Colors.white,
+                    height: 300,
+                    width: double.infinity,
+                  ),
+                )
+            ],
+          ),
         ),
       ),
     );
   }
+
+  collectedPoint({listPoint}) {
+    List<Point>? damy = [];
+
+    for (int i = 0; i < listPoint.length; i++) {
+      for (int j = 0; j < listPoint[i].length; j++) {
+        //print(listPoint[i][j][0]);
+        //print(listPoint[i][j][1]);
+        damy.add(Point(Offset(double.parse(listPoint[i][j][0]), double.parse(listPoint[i][j][1])), PointType.move, 0.0));
+      }
+    }
+    return damy;
+  }
+
 }
